00718e0241ec1750772d5ec5f4ab5bf224f89033
Yurimarin98 <76461033+Yurimarin98@users.noreply.github.com>
2021-10-12 20:12:11 -0500

    PDF CONTAINER

--- lib/src/pages/principalPage.dart
+++ lib/src/pages/principalPage.dart
@@ -18,6 +18,18 @@ class _PrincipalPageState extends State<PrincipalPage> {
     "PERIODO 3",
     "PERIODO 4",
   ];
+
+  final List<String> _titlePDF = [
+    "LIBRO MATEMATICAS",
+    "LIBRO ESPAÑOL",
+    "LIBRO INTEGRADAS",
+    "LIBRO FISICA",
+    "LIBRO QUIMICA",
+    "LIBRO RELIGION",
+    "LIBRO CONTADURIA",
+    "LIBRO BIOLOGIA",
+    "LIBRO FILOSOFIA",
+  ];
   String option = "Home";
   bool cont = true;
   VideoPlayerController videoPlayerController;
@@ -508,6 +520,9 @@ class _PrincipalPageState extends State<PrincipalPage> {
       case "Biblioteca":
         return biblioteca();
         break;
+      case "PDF":
+        return pdf();
+        break;
       default:
         return CircularProgressIndicator();
     }
@@ -575,6 +590,23 @@ class _PrincipalPageState extends State<PrincipalPage> {
     );
   }
 
+  Widget pdf() {
+    return Container(
+      color: Theme.of(context).highlightColor,
+      padding: EdgeInsets.all(10),
+      height: 450,
+      width: 700,
+      child: GridView.custom(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 5.0,
+          crossAxisSpacing: 5.0,
+        ),
+        childrenDelegate: SliverChildListDelegate(_buildItemsPDF()),
+      ),
+    );
+  }
+
   List _buildItems(int count) {
     List<Widget> listItems = List();
 
@@ -588,6 +620,25 @@ class _PrincipalPageState extends State<PrincipalPage> {
               width: 250,
               height: 400,
               image: AssetImage('assets/images/' + i.toString() + '.png'),
+            ),
+          ),
+        ),
+      );
+    }
+    return listItems;
+  }
+
+  List _buildItemsPDF() {
+    List<Widget> listItems = List();
+    for (int i = 1; i <= 10; i++) {
+      listItems.add(
+        Container(
+          padding: EdgeInsets.only(top: 15),
+          child: Center(
+            child: Image(
+              width: 250,
+              height: 400,
+              image: AssetImage('assets/images/pdf-2.png'),
             ),
           ),
         ),
